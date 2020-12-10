233d137c3d464e8d38d23f9e7deb148933445dca
Antoine Büsch <antoine.busch@gmail.com>
2020-12-10 18:23:27 +1100

    Day 9 part 2

--- src/bin/day09.rs
+++ src/bin/day09.rs
@@ -22,12 +22,32 @@ fn main() -> Result<()> {
         .map(|s| s.parse::<u64>().map_err(|e| e.into()))
         .collect::<Result<Vec<_>>>()?;
 
-    let res = nums
+    let weakness = nums
         .windows(26)
         .find_map(|window| check(&window[0..25], *window.last().unwrap()))
         .ok_or(format_err!("No solution found!"))?;
 
-    println!("part 1: {}", res);
+    println!("part 1: {}", weakness);
+
+    let mut encryption_weakness = 0;
+    for window_size in 2.. {
+        let found = nums.windows(window_size)
+            .find_map(|w| {
+                if w.iter().sum::<u64>() == weakness {
+                    let min = w.iter().min().unwrap();
+                    let max = w.iter().max().unwrap();
+                    Some(min + max)
+                } else {
+                    None
+                }
+            });
+        if let Some(res) = found {
+            encryption_weakness = res;
+            break;
+        }
+    }
+
+    println!("part 2: {}", encryption_weakness);
 
     Ok(())
 }
